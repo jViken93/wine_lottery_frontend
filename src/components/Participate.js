dad73732d0be3f0b36f3617d1ab47427dd5a7fb5
Joakim Viken <joakim.viken05@gmail.com>
2021-07-26 04:08:00 +0200

    Added registration form

---
+++ src/components/Participate.js
@@ -0,0 +1,74 @@
+import React, {useState} from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import {Form} from 'react-bootstrap'
+
+const Particpate = () => {
+    const [full_name, setFull_name] = useState('')
+    const [email, setEmail] = useState('')
+    const [tickets_to_buy, setTickets_to_buy] = useState('')
+    const [isPending, setIsPending] = useState(false)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const participant = {full_name, email, tickets_to_buy}
+        console.log(participant)
+
+        setIsPending(true); 
+
+        fetch('http://127.0.0.1:8000/LotteryRegistration/', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(participant)
+        }).then(() => {
+            console.log('new participant added');
+            setIsPending(false);
+        }) 
+    }
+
+    return (
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label for="exampleInputEmail1">Full name</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        class="form-control" 
+                        id="exampleInputEmail1" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter full name"
+                        required
+                        value = {full_name}
+                        onChange={(e) => setFull_name(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>User Email</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        class="form-control" 
+                        id="exampleInputEmail1" 
+                        aria-describedby="emailHelp" 
+                        placeholder="Enter full email"
+                        required
+                        value = {email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <Form.Text class="text-muted">We'll never share your email with anyone else.</Form.Text>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>How many ticket would you like to buy?</Form.Label>
+                    <Form.Control 
+                        type="text"
+                        required
+                        value = {tickets_to_buy}
+                        onChange={(e) => setTickets_to_buy(e.target.value)}
+                    />
+                </Form.Group>
+                { !isPending && <button class="btn btn-primary">Add participant</button>}
+                { isPending && <button class="btn btn-primary" disabled>Adding participant...</button>}
+            </Form>
+    )
+}
+
+export default Particpate
+
