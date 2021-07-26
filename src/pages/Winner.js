a5a5eaef57136a5fc81fe0fe1f0b9ebc9a40a0f3
Joakim Viken <joakim.viken05@gmail.com>
2021-07-27 01:50:00 +0200

    Added a draw winner button, and a display of all the winners

--- src/pages/Winner.js
+++ src/pages/Winner.js
@@ -1,8 +1,12 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
+import Table  from "react-bootstrap/Table";
+import Container  from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 const Winner = () => {
-    const [data, setData] = useState([]);
-     //Get Winner
+    const [data, setData] = useState('');
+    const [winners, setWinners] = useState([])
+     //Draw the winners of the competition
     const getWinner = () => {
         fetch("http://127.0.0.1:8000/tickets/get_winner/")
             .then((response) => response.json())
@@ -11,12 +15,43 @@ const Winner = () => {
                 setData(json);
         });
     };
+
+    //Get all the winners of the competition
+    useEffect(() => {
+        fetch("http://127.0.0.1:8000/tickets/get_all_winners/")
+        .then(response => response.json())
+        .then(
+            data => setWinners(data),
+            console.log(winners)
+            )
+      },[])
     
     return (
-        <div>
-            <button onClick={getWinner}>Trekk vinner!</button>
-            <h1>{data.Winner}</h1>
-    </div>
+        <Container>
+            <div>
+                <h2>Trekk en vinner</h2>
+                <Button onClick={getWinner}>Trekk vinner!</Button>
+                <br/>
+                <h3>{data.Winner}</h3>
+            </div>
+            <br/>
+            <Table striped bordered hover>
+                <thead>
+                    <tr>
+                    <th>Fullt Navn</th>
+                    <th>ID</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {winners.map(item => 
+                        <tr>
+                            <td>{item.participant}</td>
+                            <td>{item.id}</td>
+                        </tr>
+                        )}
+                </tbody>
+            </Table>
+        </Container>
     )
 }
 
